a292351378f5f2faa477acd0b98d383c155d9ae3
Jelle De Boeck <deboeck07011@gmail.com>
2024-10-04 12:38:28 +0200

    feat: add messages

--- routes/api/v1/messages.js
+++ routes/api/v1/messages.js
@@ -1,13 +1,18 @@
 // add routes
 const express = require('express');
+const text = require('jade/lib/nodes/text');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.json({ 
+  res.status(200).json({ 
     status: 'success',
     message: 'GET messages',
     data: {
-        messages: []
+        messages: [{
+            id: 1,
+            text: 'Hello World',
+            user: 'Jelle'
+        }]
     }
    });
 });

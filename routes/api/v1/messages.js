e08264a105c73c52289782b2cc264b9827cf53ec
Jelle De Boeck <deboeck07011@gmail.com>
2024-10-04 12:01:24 +0200

    feat:add GET messages

---
+++ routes/api/v1/messages.js
@@ -0,0 +1,15 @@
+// add routes
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.json({ 
+    status: 'success',
+    message: 'GET messages',
+    data: {
+        messages: []
+    }
+   });
+});
+
+module.exports = router;
